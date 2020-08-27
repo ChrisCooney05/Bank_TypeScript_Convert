ba45f6135a44b1e29641c537df8d3d73f6659488
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-27 13:43:51 +0100

    History class recreated with privage methods/variables + TS

--- src/history.ts
+++ src/history.ts
@@ -1,5 +1,50 @@
 import Transaction from "./transaction";
 
 export default class History {
-  constructor() {}
+  #transactions: Transaction[];
+  #STATEMENT_HEADER: string;
+  constructor() {
+    this.#transactions = [];
+    this.#STATEMENT_HEADER = "date || credit || debit || balance";
+  }
+
+  deposit(date: string, funds: number, balance: number): void {
+    let fundsFloat = this.#formatInteger(funds);
+    let balanceFloat = this.#formatInteger(balance);
+    this.#makeTransactionInstance(date, fundsFloat, "", balanceFloat);
+  }
+
+  withdraw(date: string, funds: number, balance: number): void {
+    let fundsFloat = this.#formatInteger(funds);
+    let balanceFloat = this.#formatInteger(balance);
+    this.#makeTransactionInstance(date, "", fundsFloat, balanceFloat);
+  }
+
+  #formatInteger(integer: number): string {
+    return integer.toFixed(2);
+  }
+
+  #makeTransactionInstance(
+    date: string,
+    credit: string,
+    debit: string,
+    balance: string
+  ): void {
+    let singleTransaction = new Transaction(date, credit, debit, balance);
+    this.#transactions.push(singleTransaction);
+  }
+
+  #pushStatementToArray(): string[] {
+    let fullHistory = this.#transactions.reverse();
+    return fullHistory.map(t => {
+      let transaction = t.toString();
+      return `\n${transaction}`;
+    });
+  }
+
+  logStatement(): void {
+    let result = this.#pushStatementToArray();
+    let formattedResult = result.join("");
+    console.log(`${this.#STATEMENT_HEADER}${formattedResult}`);
+  }
 }
