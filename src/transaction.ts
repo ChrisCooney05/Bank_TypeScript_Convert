c32dbe4a46562ea5a4669dbbad80f93f92bbcbd1
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-27 13:46:56 +0100

    Transaction class recreated with privage methods/variables + TS

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,1 +1,20 @@
-export default class Transaction {}
+export default class Transaction {
+  #date: string;
+  #credit: string;
+  #debit: string;
+  #balance: string;
+  constructor(date: string, credit: string, debit: string, balance: string) {
+    this.#date = date;
+    this.#credit = credit;
+    this.#debit = debit;
+    this.#balance = balance;
+  }
+
+  toString(): string {
+    let date = this.#date;
+    let credit = this.#credit;
+    let debit = this.#debit;
+    let balance = this.#balance;
+    return `${date} || ${credit} || ${debit} || ${balance}`;
+  }
+}
